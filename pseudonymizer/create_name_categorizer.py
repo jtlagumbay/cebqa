b993b22fc0cbf880205e1ee947b67e7a675c40d4
jtlagumbay <jtlagumbay@up.edu.ph>
2024-11-15 08:40:16 +0800

    add more surnames

---
+++ pseudonymizer/create_name_categorizer.py
@@ -0,0 +1,96 @@
+    
+import tkinter as tk
+from tkinter import messagebox
+from utils import *
+import time 
+
+GIVEN_NAME_MALE = 0
+GIVEN_NAME_FEMALE = 1
+SURNAME_OTHER = 2
+SURNAME_CH = 3
+SURNAME_MORO = 4
+NOT_NAME = 5
+GIVEN_NAME_EITHER = 6
+
+person_start = 4114
+person_index = person_start
+end_limit = 4513
+name_cat = {}
+person_names = read_file(get_path([ "data", "person_names_filtered.json"]))    
+
+root = tk.Tk()
+
+def mark_name(category):
+    print(category)
+    global person_index 
+    try:
+
+        person_name = person_names[person_index].lower()
+        print(person_name)
+        if person_index < end_limit and not person_name in name_cat:
+            name_cat[person_name] = category
+        
+            person_index += 1
+            
+        if person_index >= end_limit:
+            close_window()
+
+        # Update the label with the next name if available
+        if person_index < len(person_names):
+            name_label.config(text=f"{person_index}.) {person_names[person_index]}")
+
+    except Exception as e:
+        print(e)
+        close_window()
+
+def close_window():
+    timestamp = time.strftime("%Y%m%d-%H%M%S")
+    print("Closing application.")
+    write_file(get_path(["pseudonymizer", f"person_cat-{person_start}-{person_index}-{timestamp}"]), name_cat)
+    root.after(5000, root.destroy())
+
+
+if __name__ == "__main__":
+    root.geometry("800x600")  # Widened window to 800px width and 600px height
+    root.title("Name Identifier")
+
+    # # Label to display the name
+    name_label = tk.Label(root, text=f"{person_index}.) {person_names[person_index]}", font=("Helvetica", 16))
+    name_label.pack(pady=20, fill=tk.BOTH, expand=True)
+
+    # Buttons for Given Name and Surname
+    name_female_btn = tk.Button(root, text="(1) Female", command=lambda: mark_name(GIVEN_NAME_FEMALE))
+    name_female_btn.pack(padx=10, pady=10)
+
+    name_male_btn = tk.Button(root, text="(2) Male", command=lambda: mark_name(GIVEN_NAME_MALE))
+    name_male_btn.pack(padx=10, pady=10)
+
+    name_male_btn = tk.Button(root, text="(3) Unisex", command=lambda: mark_name(GIVEN_NAME_EITHER))
+    name_male_btn.pack(padx=10, pady=10)
+
+    surname_chinese_btn = tk.Button(root, text="(4) Chinese", command=lambda: mark_name(SURNAME_CH))
+    surname_chinese_btn.pack(padx=10, pady=10)
+
+    surname_moro_btn = tk.Button(root, text="(5) Moro", command=lambda: mark_name(SURNAME_MORO))
+    surname_moro_btn.pack(padx=10, pady=10)
+
+    surname_other_btn = tk.Button(root, text="(6) Other", command=lambda: mark_name(SURNAME_OTHER))
+    surname_other_btn.pack(padx=10, pady=10)
+
+    surname_other_btn = tk.Button(root, text="(7) Not name", command=lambda: mark_name(NOT_NAME))
+    surname_other_btn.pack(padx=10, pady=10)
+
+    close_btn = tk.Button(root, text="close", command=close_window)
+    close_btn.pack(padx=10, pady=10)
+
+    root.bind('1', lambda event: mark_name(GIVEN_NAME_FEMALE))  # "1" for Female
+    root.bind('2', lambda event: mark_name(GIVEN_NAME_MALE))    # "2" for Male
+    root.bind('3', lambda event: mark_name(GIVEN_NAME_EITHER))         # "3" for Unisex
+    root.bind('4', lambda event: mark_name(SURNAME_CH))       # "4" for Moro
+    root.bind('5', lambda event: mark_name(SURNAME_MORO))       # "5" for Moro
+    root.bind('6', lambda event: mark_name(SURNAME_OTHER))       # "6" for Other surname
+    root.bind('7', lambda event: mark_name(NOT_NAME))      # "7" for NOT Name
+ 
+
+    root.mainloop()
+
--- pseudonymizer/create_pseudonym_map.py
+++ pseudonymizer/create_pseudonym_map.py
@@ -1,96 +1,33 @@
-    
-import tkinter as tk
-from tkinter import messagebox
 from utils import *
-import time 
 
-GIVEN_NAME_MALE = 0
-GIVEN_NAME_FEMALE = 1
-SURNAME_OTHER = 2
-SURNAME_CH = 3
-SURNAME_MORO = 4
-NOT_NAME = 5
-GIVEN_NAME_EITHER = 6
+GIVEN_NAME_MALE = 0 # 1131
+GIVEN_NAME_FEMALE = 1 # 684
+SURNAME_OTHER = 2 # 2395
+SURNAME_CH = 3 # 25
+SURNAME_MORO = 4 # 9
+NOT_NAME = 5 # 261
+GIVEN_NAME_EITHER = 6 # 0
 
-person_start = 4114
-person_index = person_start
-end_limit = 4513
-name_cat = {}
-person_names = read_file(get_path([ "data", "person_names_filtered.json"]))    
+# Combine to one
+# cat1 = read_file(get_path(["pseudonymizer", "person_cat-0-500-20241113-094548"]))
+# cat2 = read_file(get_path(["pseudonymizer", "person_cat-501-1000-20241113-104817"]))
+# cat3 = read_file(get_path(["pseudonymizer", "person_cat-1000-1500-20241113-142806"]))
+# cat4 = read_file(get_path(["pseudonymizer", "person_cat-1500-2000-20241113-155600"]))
+# cat5 = read_file(get_path(["pseudonymizer", "person_cat-2000-2500-20241113-191653"]))
+# cat6 = read_file(get_path(["pseudonymizer", "person_cat-2500-3000-20241114-085840"]))
+# cat7 = read_file(get_path(["pseudonymizer", "person_cat-3000-3500-20241114-105103"]))
+# cat8 = read_file(get_path(["pseudonymizer", "person_cat-3500-4000-20241114-121101"]))
+# cat9 = read_file(get_path(["pseudonymizer", "person_cat-4000-4114-20241114-131444"]))
+# cat10 = read_file(get_path(["pseudonymizer", "person_cat-4114-4513-20241115-072108"]))
+# name_cat = {**cat1, **cat2, **cat3, **cat4, **cat5, **cat6, **cat7, **cat8, **cat9, **cat10}
+# write_file(get_path(["data", "name_category.json"]), name_cat)
 
-root = tk.Tk()
+name_cat = read_file(get_path(["data", "name_category.json"]))
+print(name_cat)
 
-def mark_name(category):
-    print(category)
-    global person_index 
-    try:
+# print([key for key, value in name_cat.items() if value == 2])
+for key, value in name_cat.items():
+    if value == 2:
+        print(key)
 
-        person_name = person_names[person_index].lower()
-        print(person_name)
-        if person_index < end_limit and not person_name in name_cat:
-            name_cat[person_name] = category
-        
-            person_index += 1
-            
-        if person_index >= end_limit:
-            close_window()
-
-        # Update the label with the next name if available
-        if person_index < len(person_names):
-            name_label.config(text=f"{person_index}.) {person_names[person_index]}")
-
-    except Exception as e:
-        print(e)
-        close_window()
-
-def close_window():
-    timestamp = time.strftime("%Y%m%d-%H%M%S")
-    print("Closing application.")
-    write_file(get_path(["pseudonymizer", f"person_cat-{person_start}-{person_index}-{timestamp}"]), name_cat)
-    root.after(5000, root.destroy())
-
-
-if __name__ == "__main__":
-    root.geometry("800x600")  # Widened window to 800px width and 600px height
-    root.title("Name Identifier")
-
-    # # Label to display the name
-    name_label = tk.Label(root, text=f"{person_index}.) {person_names[person_index]}", font=("Helvetica", 16))
-    name_label.pack(pady=20, fill=tk.BOTH, expand=True)
-
-    # Buttons for Given Name and Surname
-    name_female_btn = tk.Button(root, text="(1) Female", command=lambda: mark_name(GIVEN_NAME_FEMALE))
-    name_female_btn.pack(padx=10, pady=10)
-
-    name_male_btn = tk.Button(root, text="(2) Male", command=lambda: mark_name(GIVEN_NAME_MALE))
-    name_male_btn.pack(padx=10, pady=10)
-
-    name_male_btn = tk.Button(root, text="(3) Unisex", command=lambda: mark_name(GIVEN_NAME_EITHER))
-    name_male_btn.pack(padx=10, pady=10)
-
-    surname_chinese_btn = tk.Button(root, text="(4) Chinese", command=lambda: mark_name(SURNAME_CH))
-    surname_chinese_btn.pack(padx=10, pady=10)
-
-    surname_moro_btn = tk.Button(root, text="(5) Moro", command=lambda: mark_name(SURNAME_MORO))
-    surname_moro_btn.pack(padx=10, pady=10)
-
-    surname_other_btn = tk.Button(root, text="(6) Other", command=lambda: mark_name(SURNAME_OTHER))
-    surname_other_btn.pack(padx=10, pady=10)
-
-    surname_other_btn = tk.Button(root, text="(7) Not name", command=lambda: mark_name(NOT_NAME))
-    surname_other_btn.pack(padx=10, pady=10)
-
-    close_btn = tk.Button(root, text="close", command=close_window)
-    close_btn.pack(padx=10, pady=10)
-
-    root.bind('1', lambda event: mark_name(GIVEN_NAME_FEMALE))  # "1" for Female
-    root.bind('2', lambda event: mark_name(GIVEN_NAME_MALE))    # "2" for Male
-    root.bind('3', lambda event: mark_name(GIVEN_NAME_EITHER))         # "3" for Unisex
-    root.bind('4', lambda event: mark_name(SURNAME_CH))       # "4" for Moro
-    root.bind('5', lambda event: mark_name(SURNAME_MORO))       # "5" for Moro
-    root.bind('6', lambda event: mark_name(SURNAME_OTHER))       # "6" for Other surname
-    root.bind('7', lambda event: mark_name(NOT_NAME))      # "7" for NOT Name
- 
-
-    root.mainloop()
 
